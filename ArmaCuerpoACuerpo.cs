cb7e409a929e2259cc61064ada6e9582970e0877
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-11 16:52:43 -0500

    Created Class ArmaCuerpoACuerpo
    -Tambien modifique la clase padre Arma: ya que  esta clase no era publica.

---
+++ ArmaCuerpoACuerpo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_2Taller3DSemana03Tarea02
+{
+    public class ArmaCuerpoACuerdo : Arma
+    {
+        public ArmaCuerpoACuerdo(string nombre, double daño, double velocidadAtaque, double precio)
+            : base(nombre, daño, velocidadAtaque, precio) { }
+    }
+}
